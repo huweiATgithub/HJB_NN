2026735292ceadc4b7e7629be967669f9f3ba9a6
HU Wei <whuae@connect.ust.hk>
2020-06-16 18:20:35 +0800

    Add Burgers model

---
+++ utilities/other.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def cheb(N):
+    '''Build Chebyshev differentiation matrix.
+    Uses algorithm on page 54 of Spectral Methods in MATLAB by Trefethen.'''
+    theta = np.pi / N * np.arange(0, N+1)
+    X_nodes = np.cos(theta)
+
+    X = np.tile(X_nodes, (N+1, 1))
+    X = X.T - X
+
+    C = np.concatenate(([2.], np.ones(N-1), [2.]))
+    C[1::2] = -C[1::2]
+    C = np.outer(C, 1./C)
+
+    D = C / (X + np.identity(N+1))
+    D = D - np.diag(D.sum(axis=1))
+
+    # Clenshaw-Curtis weights
+    # Uses algorithm on page 128 of Spectral Methods in MATLAB
+    w = np.empty_like(X_nodes)
+    v = np.ones(N-1)
+    for k in range(2, N, 2):
+        v -= 2.*np.cos(k * theta[1:-1]) / (k**2 - 1)
+
+    if N % 2 == 0:
+        w[0] = 1./(N**2 - 1)
+        v -= np.cos(N*theta[1:-1]) / (N**2 - 1)
+    else:
+        w[0] = 1./N**2
+
+    w[-1] = w[0]
+    w[1:-1] = 2.*v/N
+
+    return X_nodes, D, w
